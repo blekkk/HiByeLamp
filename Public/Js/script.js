3da95a0f24628e6306dffea6ea1a73590ef0812c
blekkk <fazailman992@gmail.com>
2021-05-02 20:44:40 +0700

    change content to carousel

--- Public/Js/script.js
+++ Public/Js/script.js
@@ -1,7 +1,20 @@
-var swiper = new Swiper('.swiper-container', {
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+var swiperMain = new Swiper('.main-carousel', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+var swiperArrivals = new Swiper('.content-carousel', {
+  slidesPerView: window.screen.width >= 768? 5 : 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
